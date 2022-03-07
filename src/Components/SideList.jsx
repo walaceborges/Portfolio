98dd849530c684d15c57ed5970bea33207c5145b
Walace <walacewab@gmail.com>
2022-03-07 02:23:02 -0300

    style: altera a lista lateral de redes sociais

--- src/Components/SideList.jsx
+++ src/Components/SideList.jsx
@@ -1,38 +1,38 @@
 import React from "react";
-import listGitHub from "../assets/images/list-github.svg";
-import listLinkedin from "../assets/images/list-linkedin.svg";
-import listResume from "../assets/images/list-resume.svg";
+import listGitHub from "../assets/images/github-with-circle-svgrepo-com.svg";
+import listLinkedin from "../assets/images/linkedin-svgrepo-com.svg";
+import listResume from "../assets/images/curriculum-svgrepo-com.svg";
 
 function SideList() {
   return (
     <div className="relative top-60 hidden xl:block">
       <div className="flex justify-end inset-0">
         <ul className="z-10 fixed h-0 pr-8">
-          <li className="my-4">
+          <li className="my-5">
             <a
               href="https://github.com/walaceborges"
               target="_blank"
               rel="noreferrer"
             >
-              <img className="w-16" src={listGitHub} alt="github" />
+              <img className="w-16 transition ease-in-out delay-100 hover:scale-110 duration-300 hover:brightness-200" src={listGitHub} alt="github" />
             </a>
           </li>
-          <li className="my-4">
+          <li className="my-5">
             <a
               href="https://www.linkedin.com/in/walace-borges/"
               target="_blank"
               rel="noreferrer"
             >
-              <img className="w-16" src={listLinkedin} alt="linkedin" />
+              <img className="w-16 transition ease-in-out delay-100 hover:scale-110 duration-300 hover:brightness-150" src={listLinkedin} alt="linkedin" />
             </a>
           </li>
-          <li className="my-7">
+          <li className="my-5">
             <a
               href="https://github.com/walaceborges"
               target="_blank"
               rel="noreferrer"
             >
-              <img className="w-16" src={listResume} alt="github" />
+              <img className="w-16 transition ease-in-out delay-100 hover:scale-110 duration-300 hover:brightness-125" src={listResume} alt="github" />
             </a>
           </li>
         </ul>
