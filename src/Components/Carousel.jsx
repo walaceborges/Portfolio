165f3bb19defbfa9cbbde68ac7481d2a7d5d24cc
Walace <walacewab@gmail.com>
2022-03-09 12:16:30 -0300

    fix: remove a tag <a> por completa dos projetos inativos

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -85,30 +85,18 @@ function Carousel() {
             <p class="my-3 font-normal text-sm text-violet-600 text-center md:text-lg">
               # # # #
             </p>
-            <a
-              href="https://github.com/walaceborges/adzuna"
-              target="_blank"
-              rel="noreferrer"
-            >
               <button
                 type="button"
                 class="text-white bg-slate-800 hover:bg-slate-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               >
                 Codigo
               </button>
-            </a>
-            <a
-              href="https://ejobs.vercel.app/#"
-              target="_blank"
-              rel="noreferrer"
-            >
               <button
                 type="button"
                 class="text-white bg-slate-800 hover:bg-slate-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               >
                 Review
               </button>
-            </a>
           </div>
         </div>
       </SwiperSlide>
@@ -128,30 +116,18 @@ function Carousel() {
             <p class="my-3 font-normal text-sm text-violet-600 text-center md:text-lg">
               # # # #
             </p>
-            <a
-              href="https://github.com/walaceborges/adzuna"
-              target="_blank"
-              rel="noreferrer"
-            >
               <button
                 type="button"
                 class="text-white bg-slate-800 hover:bg-slate-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               >
                 Codigo
               </button>
-            </a>
-            <a
-              href="https://ejobs.vercel.app/#"
-              target="_blank"
-              rel="noreferrer"
-            >
               <button
                 type="button"
                 class="text-white bg-slate-800 hover:bg-slate-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               >
                 Review
               </button>
-            </a>
           </div>
         </div>
       </SwiperSlide>
